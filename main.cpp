bc763a0309f458426171c08ee0d3b3eac3ca9f43
Kulakov Ilia <kulakov@selectel.ru>
2026-08-10 08:51:12 +0300

    Add linter job
    * add new job linter
    * format files with clang-format
    * separate release and build flows

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include "lib.h"
-
 #include <iostream>
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+#include "lib.h"
+
+int main (int, char**)
+{
+    std::cout << "Version: " << version () << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
