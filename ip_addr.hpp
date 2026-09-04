848c0f8530ace4831c4ca99a27da073ccff5fcae
Kulakov Ilia <kulakov@selectel.ru>
2026-09-04 14:12:48 +0300

    SFINAE

--- ip_addr.hpp
+++ ip_addr.hpp
@@ -45,7 +45,7 @@ inline constexpr bool is_container_v = is_container<T>::value;
  * @param ip_addr The value to print.
  */
 template <typename IP>
-void print_ip (IP ip_addr)
+void print_ip_ce (IP ip_addr)
 {
     if constexpr (std::is_same_v<IP, std::string>)
     {
@@ -96,4 +96,48 @@ void print_ip (const std::tuple<Args...>& ip_addr)
             std::cout << '\n';
         },
         ip_addr);
+}
+
+// SFINAE
+
+template <bool cond, class T>
+struct enable_if
+{
+    using type = T;
+};
+
+template <class T>
+struct enable_if<false, T>
+{
+};
+
+template <typename IP, typename = typename enable_if<std::is_same<IP, std::string>::value, IP>::type>
+void print_ip (IP ip_addr)
+{
+    std::cout << ip_addr << "\n";
+}
+
+template <typename IP,
+          typename std::enable_if<std::is_integral<IP>::value && !std::is_same<IP, std::string>::value, int>::type = 0>
+void print_ip (IP ip_addr)
+{
+    for (size_t i = sizeof (IP); i-- > 0;)
+    {
+        std::cout << (i + 1 == sizeof (IP) ? "" : ".") << static_cast<int> (ip_addr >> (i * 8) & 0xFF);
+    }
+    std::cout << '\n';
+}
+
+template <typename IP, typename enable_if<is_container_v<IP> && !std::is_same<IP, std::string>::value, int>::type = 0>
+void print_ip (const IP& ip_addr)
+{
+    bool first = true;
+    for (auto& octet : ip_addr)
+    {
+        if (!first)
+            std::cout << ".";
+        std::cout << octet;
+        first = false;
+    }
+    std::cout << '\n';
 }
